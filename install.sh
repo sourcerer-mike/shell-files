4d2ab7e55016b799b3fd0cd61ed89912c46e10b5
Mike Pretzlaw <pretzlaw@gmail.com>
2014-08-29 14:57:32 +0200

    Better install script with chown

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+USER_GROUP=$(id -Gn mike | cut -d" " -f1)
+USER_NAME=`whoami`
+
+VERBOSE=1
+OVERWRITE=0
+
+echo "Install for ${USER_NAME}:${USER_GROUP}"
+
+function verbose()
+{
+  [[ ${VERBOSE} ]] && echo ".$@"
+}
+
 for F in $(find -L . -mindepth 2 -type f | grep -v "^\./\."); do # look up and symlink each file
     TARGET=`echo ${F} | sed 's/^\.//'`;
     if [[ $TARGET == /home/me/* ]]; then # place correct username for home dir
@@ -12,7 +25,13 @@ for F in $(find -L . -mindepth 2 -type f | grep -v "^\./\."); do # look up and s
         mkdir -p $TARGET_DIR
     fi
 
-    if [ ! -f ${TARGET} ]; then
+    if [[ ${OVERWRITE} ]]; then
+        sudo rm ${TARGET}
+    fi
+
+    if [[ ! -f ${TARGET} ]]; then
+        verbose ${TARGET}
         sudo ln -s `readlink -f ${F}` ${TARGET}
+        sudo chown ${USER_NAME}:${USER_GROUP} ${TARGET}
     fi
 done
